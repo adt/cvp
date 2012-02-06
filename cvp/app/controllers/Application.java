2660895d16c259739d3fb57a3649be7865c79f17
adt <abhimanyureddy28@gmail.com>
2012-02-06 13:39:54 +0530

    first commit

---
+++ cvp/app/controllers/Application.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import play.*;
+
+import play.mvc.*;
+
+import java.util.*;
+
+
+import models.*;
+
+
+public class Application extends Controller
+ {
+
+   
+public static void home() {
+        render();
+    }
+public static void index() {
+        render();
+    }
+
+    
+ 
+
+public static void cvp() {
+        render();
+    }
+
+public static void why() {
+        render();
+    }
+
+
+
+public static void contacts() {
+        render();
+        }
+
+public static void about() {
+        render();
+    } 
+
+public static void signup() {
+        render();
+    }
+
+
+
+}
